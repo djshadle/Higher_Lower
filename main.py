694193c7c1b6d2961d259e5b09989d305927ec0d
djshadle <63021869+djshadle@users.noreply.github.com>
2021-11-25 03:19:18 +0000

    updated logic to move choice two to choice one at end of loop.

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-# from random import randint
-# from replit import clear
-# from game_data import data
-# from art import logo, vs
+from random import randint
+from replit import clear
+from game_data import data
+from art import logo, vs
 
-# def game_loop():
-#   game_data = data
-#   number_in_list = (len(game_data)-1)
-#   should_run = True
-#   score = 0
+def game_loop():
+  game_data = data
+  number_in_list = (len(game_data)-1)
+  should_run = True
+  score = 0
 
-  def two_variables(game_data):
+  def variable_two(game_data):
     """ Use game_data to select two random variables """
-    random_position_one = randint(0, number_in_list)
     random_position_two = randint(0, number_in_list)
     while random_position_two == random_position_one:
       random_position_two = randint(0, number_in_list)
-    first_choice = game_data[random_position_one]
     second_choice = game_data[random_position_two]
-    return first_choice, second_choice
+    return second_choice
 
   def higher_lower(variable_one, variable_two, score):
     user_choice = input("---------\nWho has more followers? Type 'A' or 'B': ").lower()
@@ -34,20 +32,24 @@
     else:
       print(f"Sorry, that's wrong. Final score: {score}")
       return False, score
+  
+  random_position_one = randint(0, number_in_list)
+  first_choice = game_data[random_position_one]
+  choice_one = first_choice
+  
 
   while should_run:
     clear()
-    choices = two_variables(game_data)
-    choice_one = choices[0]
-    choice_two = choices[1]
+    choice_two = variable_two(game_data)
     print(logo)
     print(f"Current score: {score}")
     print(f"Compare A: {choice_one['name']}, a {choice_one['description']}, from {choice_one['country']}.")
     print(vs)
     print(f"Against B: {choice_two['name']}, a {choice_two['description']}, from {choice_two['country']}.")
-    game_check = higher_lower(choices[0], choices[1], score)
+    game_check = higher_lower(choice_one, choice_two, score)
     should_run = game_check[0]
     score = game_check[1]
+    choice_one = choice_two
 
   if input("\nDo you want to play again? 'Y' or 'N': ").lower() == 'y':
     game_loop()
